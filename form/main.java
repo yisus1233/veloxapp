e77dcd44f066b06e68a07aa396c0a5a64db3ee13
CESAR <U23304028@UTP.EDU.PE>
2025-07-17 02:51:02 -0500

    PRUEVA 1

--- form/main.java
+++ form/main.java
@@ -1,7 +1,12 @@
-package veloxapp.form;
+package veloxapp;
+
+import veloxapp.form.ClienteForm;
 
 public class main {
     public static void main(String[] args) {
-        new ClienteForm().setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            ClienteForm form = new ClienteForm();
+            form.setVisible(true);
+        });
     }
 }
