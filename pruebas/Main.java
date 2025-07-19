c714149fc180807b3dd982919fc095e1785f6951
DAVID <1612074@utp.edu.pe>
2025-07-19 09:32:42 -0500

    davidluque mision cumplida

--- pruebas/Main.java
+++ pruebas/Main.java
@@ -1,6 +1,6 @@
 package pruebas;
 
-import veloxapp.form.PedidoForm;
+import veloxapp.form.EntregaForm;
 import javax.swing.UIManager;
 
 public class Main {
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("No se pudo aplicar el estilo visual.");
         }
 
-        PedidoForm ventana = new PedidoForm();
+        EntregaForm ventana = new EntregaForm();
         ventana.setVisible(true);
     }
 }
